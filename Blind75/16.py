5023829c07b7322986de0f8bfd2bad4a719b0e1a
BhushanChowta <bhushanchowta@gmail.com>
2024-02-29 09:07:32 +0530

    16

---
+++ Blind75/16.py
@@ -0,0 +1,24 @@
+#409
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        freq={}
+        for c in s:
+            freq[c]=freq.get(c,0)+1
+        
+        len=0
+        hasOddFreq=False
+
+        for count in freq.values():
+            if count % 2 == 0:
+                len+=count
+            else:
+                if not hasOddFreq:
+                    len+=count
+                    hasOddFreq=True
+                else:
+                    len+=count-1
+        return len
