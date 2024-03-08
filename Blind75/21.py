bb2e2f04080a675a7b6796b36b44198bc29b40b8
BhushanChowta <bhushanchowta@gmail.com>
2024-03-08 07:57:13 +0530

    21

---
+++ Blind75/21.py
@@ -0,0 +1,19 @@
+#876
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        f,s=head,head
+
+        while f and f.next:
+            f=f.next.next
+            s=s.next
+
+        return s
