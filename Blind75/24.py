bfc558394c52ace5d02afd8a808ee352b392748e
BhushanChowta <bhushanchowta@gmail.com>
2024-03-11 08:31:05 +0530

    24

---
+++ Blind75/24.py
@@ -0,0 +1,17 @@
+#53
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        
+        cs=ms=nums[0]
+
+        for num in nums[1:]:
+            cs=max(num,cs+num)
+            ms=max(cs,ms)
+        
+        return ms
