6cdd0e61c202542632597fd875661fc1f831e5b1
BhushanChowta <bhushanchowta@gmail.com>
2024-04-07 18:44:06 +0530

    231

---
+++ Random/231.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        
+        # Keep dividing n by 2 until it becomes odd
+        while n % 2 == 0:
+            n //= 2
+        
+        # After division, if n becomes 1, it's a power of two
+        return n == 1
