2aa91a162886003da41cca37bffe7ffe18d0c982
BhushanChowta <bhushanchowta@gmail.com>
2024-05-06 19:56:19 +0530

    2487

---
+++ Random/2487.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNodes(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if not head: return None
+        head.next = self.removeNodes(head.next)
+        if head.next and head.val < head.next.val:
+            #f the current node's value is less than the next node's value,
+            # it updates the current node's next pointer to skip the next node.
+            return head.next
+        return head
+        
