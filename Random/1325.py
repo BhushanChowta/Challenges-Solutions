b72ebe0a1401558fdfbb252f134a8b1856aab0b4
BhushanChowta <bhushanchowta@gmail.com>
2024-05-20 08:34:07 +0530

    fix

--- Random/1325.py
+++ Random/1325.py
@@ -4,4 +4,4 @@ class Solution(object):
             root.left = self.removeLeafNodes(root.left, target)
             root.right = self.removeLeafNodes(root.right, target)
             if root.val != target or root.left or root.right:
-                return root
+                return root 
