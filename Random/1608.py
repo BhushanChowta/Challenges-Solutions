3092c2fbc29ac1784979a7beb682e64214c962f6
BhushanChowta <bhushanchowta@gmail.com>
2024-05-27 08:40:08 +0530

    1608

---
+++ Random/1608.py
@@ -0,0 +1,9 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        nums_len = len(nums)
+        for x in range(nums_len + 1):
+            # Count how many numbers are greater than or equal to x
+            cnt = sum(1 for num in nums if num >= x)
+            if cnt == x:
+                return x
+        return -1
